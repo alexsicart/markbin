991a6b085d3ffe731ee3b2dc8d00df374c3265d5
alexsicart <alex@sharge.io>
2017-08-26 12:19:54 +0200

    feat: filtering access

--- server/main.js
+++ server/main.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+import { Bins } from '../imports/collections/bins';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    Meteor.publish('bins', function() {
+        //Filtering the access to one user
+      return Bins.find({ ownerId: this.userId });
+    });
 });
